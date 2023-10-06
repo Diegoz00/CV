b5216f89cddd2af0aaee4dbea0b899ea1ed5024a
diegoz00 <diegozalazar00@gmail.com>
2023-10-06 13:40:27 -0300

    entrega proyecto final CV

--- js/function.js
+++ js/function.js
@@ -1,8 +1,8 @@
 
 //No me esta funcionando el cambio segun video ejemplo del curso
 
-document.getElementById("fondo").addEventListener("click", function () {
-    document.body.style.backgroundColor = "black";    
-});
-
+document.getElementById('fondo').onclick = function () {
+    document.body.style.backgroundColor = "black"; 
+    window.onload=hacerclic;   
+}
 

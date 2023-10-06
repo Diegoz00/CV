b8e735a3ddcae364983077c707cd8166fcbe37bf
diegoz00 <diegozalazar00@gmail.com>
2023-10-06 09:51:10 -0300

    estilos y datos requeridos del formulario

--- function.js
+++
@@ -1,4 +0,0 @@
-
-document.getElementById("fondo").addEventListener("click", function () {
-    document.body.style.backgroundColor = "red";    
-});
---
+++ js/function.js
@@ -0,0 +1,8 @@
+
+//No me esta funcionando el cambio segun video ejemplo del curso
+
+document.getElementById("fondo").addEventListener("click", function () {
+    document.body.style.backgroundColor = "black";    
+});
+
+
